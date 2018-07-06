11d79f463d299bc11c18f44aeb27ada872b1b868
YAWAL <gajkaGAJKA140889>
2018-07-06 12:57:48 +0300

    working with lib

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"github.com/YAWAL/CryptoTrader/gdaxClient"
 	"os"
 	"fmt"
+	"github.com/preichenberger/go-gdax"
 )
-
-type Product struct {
-	Id             string `json:"id"`
-	BaseCurrency   string `json:"base_currency"`
-	QuoteCurrency  string `json:"quote_currency"`
-	BaseMinSize    string `json:"base_min_size"`
-	BaseMaxSize    string `json:"base_max_size"`
-	QuoteIncrement string `json:"quote_increment"`
-
-	CancelOnly     bool   `json:"cancel_only"`
-	DisplayName    string `json:"display_name"`
-	LimitOnly      bool   `json:"limit_only"`
-	MarginEnable   string   `json:"margin_enable"`
-	MaxMarketFunds string `json:"max_market_funds"`
-	MinMarketFunds string `json:"min_market_funds"`
-	PostOnly       bool   `json:"post_only"`
-	Status         string `json:"status"`
-	StatusMessage  string `json:"status_message"`
-}
-
-//type prod []Product
 
 func main() {
 
@@ -33,21 +12,30 @@ func main() {
 	key := os.Getenv("COINBASE_KEY")
 	passphrase := os.Getenv("COINBASE_PASSPHRASE")
 
-	client := gdaxClient.NewClient(secret, key, passphrase)
+	//client := gdaxClient.NewClient(secret, key, passphrase)
 
-	products := []Product{}
+	cl := gdax.NewClient(secret,key,passphrase)
 
-	res, err := client.Request("GET", "/products", nil, &products)
+	//products, err := handlers.GetProducts(client)
+	products, err := cl.GetProducts()
+
 	if err != nil {
 		fmt.Println("1 : ", err)
-		return
 	}
-	res.Header.Set("Content-Type", "application/json")
 
-	defer res.Body.Close()
 	for _, prod := range products {
 		fmt.Println(prod.Id)
 
 	}
 
+	//book, err := handlers.GetBook(client, "BTC-USD",1)
+	book, err := cl.GetBook("BCH-EUR",1)
+
+	if err != nil {
+		fmt.Println("2 : ", err)
+	}
+		fmt.Println(book)
+
 }
+
+
